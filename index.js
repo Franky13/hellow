b94afdec9240bb023638bf2366c25b222c819d1c
Алиса Грачева <alisagraceva@MacBook-Pro.local>
2019-11-18 05:47:35 +0300

    filter commands

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ bot.on(/^\/say (.+)$/, (msg, props) => {
     return bot.sendMessage(msg.from.id, text, { replyToMessage: msg.message_id });
 });
 bot.on('text', (msg) => {
+    if (!msg.text.startsWith('/')){
     fs.writeFileSync('/tmp/test', msg.text);
     return bot.sendMessage(msg.from.id, 'Молодец');
-    });
+    }});
 bot.on('/show', (msg) => {
     const text = fs.readFileSync('/tmp/test');
     return bot.sendMessage(msg.from.id, text);
