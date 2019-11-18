e5aab990f7d4cffcac3f3f423421a52b9e4e369d
Алиса Грачева <alisagraceva@MacBook-Pro.local>
2019-11-18 04:47:07 +0300

    Welcome

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const TeleBot = require('telebot');
 const bot = new TeleBot('1004574146:AAFQOcz8-1KxGdjrMfi-MrOv9xGKoI-v18c');
 bot.on('text', (msg) => msg.reply.text(msg.text+'hello'));
+bot.on(['/start', '/hello'], (msg) => msg.reply.text('Welcome!'));
 bot.start();
-
