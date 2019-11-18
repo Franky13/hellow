4b08dc867265a739af229e23fafe8352661d5ece
Алиса Грачева <alisagraceva@MacBook-Pro.local>
2019-11-18 04:57:16 +0300

    Reply to message

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const TeleBot = require('telebot');
 const bot = new TeleBot('1004574146:AAFQOcz8-1KxGdjrMfi-MrOv9xGKoI-v18c');
 bot.on(['/start', '/hello'], (msg) => msg.reply.text('Welcome!'));
+bot.on(/^\/say (.+)$/, (msg, props) => {
+    const text = props.match[1];
+    return bot.sendMessage(msg.from.id, text, { replyToMessage: msg.message_id });
+});
 bot.start();
